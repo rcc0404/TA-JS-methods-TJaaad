70b0c787c56771b6956c0f370551b7a633be0eb3
rcc0404 <rahulchaudhar3@gmail.com>
2022-06-01 08:11:01 +0530

    Asignment no 2

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -12,7 +12,7 @@ let words = [
 // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
 
 // - Convert the above array "words" into an array of length of word instead of word.
-
+.
 // - Create a new array that only contains word with atleast one vowel.
 
 // - Find the index of the word "rhythm"
